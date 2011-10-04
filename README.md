ace4f8b121be2caa4271d5c1da0c5b038f2ceed2
Marco Rondini <m.rondini@tigersecurity.it>
2011-10-04 13:42:37 +0200

    first commit of web backdoor finder light

---
+++ README.md
@@ -0,0 +1,9 @@
+WEB BACKDOOR FINDER LIGHT
+=========================
+
+How to run:
+___________
+
+ - Example: `./wbf.sh /dir/to/php/pages '*.php'` 
+
+
---
+++ wbfl.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+help__() { 
+        echo 
+        echo "USAGE: ${0} directory 'filename'"
+	echo "Example: ${0} ./ '*.php'" 
+}
+
+: ${1? "Dir missing $(help__)"}
+: ${2? "filename missing $(help__)"}
+
+DIR=$1
+name=$2
+
+find $DIR -name "$2" -exec awk -v filename={} '{
+                                        if ( $0 ~ /([\s]+|=|&|@|^)(system|passthru|exec|popen|shell_exec|proc_open)[\s]*\(/ )
+                                                print "command exec - "filename": "count" - "$0
+
+                                        if ( $0 ~ /(base64_decode|base64_encode)[\s]*\(/ )
+                                                print "base64 -"filename": "count" - "$0
+
+                                        if ( $0 ~ /([\s]+|=|&|@|^)(eval)[\s]*\(/ )
+                                                print "php exec - "filename": "count" - "$0
+
+                                        count++
+                                }' {} \;
+
